bcdf2d5fe186b209ba68323a438be93b982a6961
SvenRehm <rehm21@web.de>
2022-11-13 15:37:29 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+   reactStrictMode: true,
+
+   images: {
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "images2.imgbox.com",
+            port: "",
+            pathname: "",
+         },
+      ],
+   },
 }
 
 module.exports = nextConfig
